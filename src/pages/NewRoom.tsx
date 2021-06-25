19a480948948a792e9a577812ad47cd6c5561c6d
Giuliano Crivelli <giulainocrivelli@hotmail.com>
2021-06-24 20:23:21 -0400

    added navigation to Home and NewRoom

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom";
+
 import { Button } from "../components/Button";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
-import googleIconImg from "../assets/images/google-icon.svg";
 
 import "../styles/auth.scss";
 
@@ -26,7 +27,7 @@ export function NewRoom() {
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <a href="#">clique aqui</a>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
           </p>
         </div>
       </main>
