5c062624769f8a221226e8547bda5c96ac27f8d8
thefalked <giulianocrivelli@hotmail.com>
2021-06-25 23:11:27 -0400

    adding like function on questions

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
+
 import { database } from "../services/firebase";
+
+import { useAuth } from "./useAuth";
 
 type FirebaseQuestions = Record<
   string,
@@ -11,6 +14,9 @@ type FirebaseQuestions = Record<
     content: string;
     isHighlighted: boolean;
     isAnswered: boolean;
+    likes: Record<string, {
+      authorId: string;
+    }>
   }
 >;
 
@@ -23,9 +29,12 @@ type QuestionType = {
   content: string;
   isHighlighted: boolean;
   isAnswered: boolean;
+  likeCount: number;
+  likeId: string | undefined;
 };
 
 export function useRoom(roomId: string) {
+  const { user } = useAuth();
   const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState("");
 
@@ -44,6 +53,8 @@ export function useRoom(roomId: string) {
             author: value.author,
             isHighlighted: value.isHighlighted,
             isAnswered: value.isAnswered,
+            likeCount: Object.values(value.likes ?? {}).length,
+            likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
           };
         }
       );
@@ -51,7 +62,11 @@ export function useRoom(roomId: string) {
       setTitle(databaseRoom.title);
       setQuestions(parsedQuestions);
     });
-  }, [roomId]);
+
+    return () => {
+      roomRef.off("value");
+    }
+  }, [roomId, user?.id]);
 
   return { questions, title };
 }
